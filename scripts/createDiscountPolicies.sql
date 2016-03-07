347e787ea568f965664086b78bf441f8fc592a39
piotrcierpich <piotr.cierpich@gmail.com>
2016-03-07 19:00:33 +0100

    sync single table inheritance

---
+++ scripts/createDiscountPolicies.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DiscountPolicies] (
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [DayDate]            DATETIME       NULL,
+    [FromTo_Start]       DATETIME       NULL,
+    [FromTo_End]         DATETIME       NULL,
+    [Discriminator]      NVARCHAR (MAX) NULL,
+    [DiscountPercentage] INT NULL,
+    CONSTRAINT [PK_dbo.DiscountPolicies] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
---
+++ scripts/createDiscounts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Discounts] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [Product_Id] INT NULL
+    CONSTRAINT [PK_dbo.Discounts] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Discounts_dbo.Products_Product_Id] FOREIGN KEY ([Product_Id]) REFERENCES [dbo].[Products] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Product_Id]
+    ON [dbo].[Discounts]([Product_Id] ASC);
